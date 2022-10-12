7d90ef7d3b0db9dd4006eab807857a759e60dc48
AlexMednov <alex.mednov@student.nhlstenden.com>
2022-10-12 16:41:50 +0200

    Made a header for the home page and made some organisational changes

---
+++ php_files/home_page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<link rel = "stylesheet" type = "text/css" href = "../css_files/style_home_page.css">
+		<title>Home page</title>
+	</head>
+	<body>
+		<div id = "grid_container">
+			<div id = "white_space">
+			</div>
+			<!--Have not came up with a better way to make white space-->
+			<header>
+				<img src = "../img/sunny_logos-01.png">
+				<nav>
+					<input type="button" value="Products" onclick="window.location='#'" class = "Button">
+					<input type="button" value="Return form" onclick="window.location='#'" class = "Button">
+					<input type="button" value="Contact us" onclick="window.location='#'" class = "Button">
+				</nav>
+			</header>
+			<p>Text1_colourful</p>
+			<p>Text2_durable</p>
+			<p>Text3_international</p>
+			<p>Footer</p>
+		</div>
+	</body>
+</html>
