152267628b03bb96fa7718696f0241f3873c4935
AlexMednov <alex.mednov@student.nhlstenden.com>
2022-10-13 17:00:31 +0200

    Done a bit of extra work on the main page, added fonts and smth else

--- php_files/home_page.php
+++ php_files/home_page.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset = "utf-8">
 		<link rel = "stylesheet" type = "text/css" href = "../css_files/style_home_page.css">
-		<title>Home page</title>
+		<title>Sunny socks</title>
 	</head>
 	<body>
 		<div id = "grid_container">
@@ -16,9 +16,22 @@
 					<input type="button" value="Products" onclick="window.location='#'" class = "Button">
 					<input type="button" value="Return form" onclick="window.location='#'" class = "Button">
 					<input type="button" value="Contact us" onclick="window.location='#'" class = "Button">
+					<!--Buttons that lead to diffrent pages. No diffrent pages yet, so they dont lead anywhere
+					yet.-->
 				</nav>
 			</header>
-			<p>Text1_colourful</p>
+			<div id = "box_colorfull">
+				<img src = "../img/im_sock_home.png" alt = "Sock" id = "im_sock_home">
+				<div id = "text_box_col">
+					<h1>Colourful</h1>
+					<h2>We like to add some colour to one of the the most 
+					everyday moments of life: putting on your socks. 
+					We add literal color by designing simple basic 
+					white socks a pop of colour.
+					That is why you can commission socks of any colour!
+					In addition to always available colours.</h2>
+				</div>
+			</div>
 			<p>Text2_durable</p>
 			<p>Text3_international</p>
 			<p>Footer</p>
