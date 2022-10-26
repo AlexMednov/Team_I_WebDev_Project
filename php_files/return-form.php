c094d4578d23635db2592c0f9ac246669b9ffef0
Mich Ioan <ioan.mich@student.nhlstenden.com>
2022-10-26 16:32:52 +0200

    First verion of Return Form

---
+++ php_files/return-form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Sunny Socks
+		</title>
+		<link rel="stylesheet" href="../css_files/style-return_form.css" >
+	</head>
+	
+	<body>
+		<div class="container">
+			<div id="forms">
+				<button>Classic uni color</buton>
+				<button>Classic stripes</button>
+			</div>
+			
+			<div id="right">
+				
+				<div id="info">
+				All fields are mandatory to be filled. For additional information, you could call us from 12:00 - 18:00 CET, or write us an email
+				</div>
+				
+				<div id="buttons">
+					<button id="contact_us"> Contact us</button>
+					<button id="back"> Back</button>
+				</div>
+			</div>
+		</div>
+	
+</html>
