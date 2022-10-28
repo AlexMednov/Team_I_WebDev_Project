1d019b538105ed193a4498f618af754f25298d83
sadeghimehdi <mehdi.sadeghi@student.nhlstenden.com>
2022-10-28 15:24:39 +0200

    all done

--- php_files/order.php
+++ php_files/order.php
@@ -4,27 +4,26 @@
 		<title>Order now</title>
 		<meta charset="UTF-8">
 		<meta name="description" content="Buy colourful socks from sunny">
-		<meta name="keywords" contant="socks, buy, color">
 		<meta name="author" content="Alicia Rodriguez">
 		<link rel="icon" href="../img/sunny_logos_orange.png">
-		<link rel="stylesheet" type="text/css" href="../css_files/style_order.css"> 
+		<link rel="stylesheet" type="text/css" href="../css_files/style_order.css">
 	</head>
-	
+
 	<body>
 		<div id="container">
-			<a class="button1" href="../php_files/index-product.html"> 
-				<img src="../img/previous.png" alt="button">
+			<a href="../php_files/index-product.html">
+				<img class="button1" src="../img/previous.png" alt="button">
 			</a>
 			<p class= "resellers">Resellers</p>
 			<div class="box">
 				<h1><b>Order now</b></h1>
-			
-				
+
+
 				<div class="hema">
 					<p class="title">Hema (Dutch)</p>
 					<p class="linkhema">hema.duch@gmail.com</p>
 				</div>
-				<div class="international">	
+				<div class="international">
 					<p class="title">Amazon (international)</p>
 					<p class="linkint">https./Amazon.SunnySocks.com</p>
 				</div>
@@ -32,21 +31,21 @@
 					<p class="title">Amazon (Germany)</p>
 					<p class="linkde">https:/Amazon.SunnySocks.de</p>
 				</div>
-				<div class="bol">	
+				<div class="bol">
 					<p class="title">Bol.com (Dutch)</p>
 					<p class="linkbol">https:/Bol.com.SunnySocks</p>
 				</div>
 			</div>
-			
+
 			<div class="logo">
-				<img src="../img/sunny_logos_slogan_orange.png" alt="orangelogo" width="300" height="115">
+				<img src="../img/sunny_logos_slogan_orange.png" alt="orangelogo" style="width: 300px;  height: 115px;" >
 			</div>
-			<img class="whitelogo" src="../img/sunny_logos-01.png" alt="whitelogo" width="300" height="115">
-		
+			<img class="whitelogo" src="../img/sunny_logos-01.png" alt="whitelogo" style="width: 300px;  height: 115px;">
+
 			<div class="button">
 				<h2><b><a class='back' href='index-product.html'>Return to products</a></b></h2>
-			</div>	
-				
+			</div>
+
 		</div>
 	</body>
 </html>
