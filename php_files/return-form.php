e8421d99365cd28434abdcafebd70fc9c2618480
sadeghimehdi <mehdi.sadeghi@student.nhlstenden.com>
2022-10-28 17:22:09 +0200

    FINAAAALY

--- php_files/return-form.php
+++ php_files/return-form.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="EN">
 	<head>
-		<title> 
+		<title>
 			Sunny Socks
 		</title>
 		<link rel="stylesheet" href="../css_files/style-return_form.css">
@@ -10,83 +10,93 @@
 		<meta name="keywords" content="Socks, International, Return">
 		<meta name="author" content="Ioan Mich">
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
-	
+
 	</head>
 	<body>
 		<a class="icon" href="home_page.php">
               <img src="../img/previous.png" alt="">
          </a>
-		
+
 		<div class="Text">
 			<p> Return Form </p>
 		</div>
-		
+
 		<div class="container">
 
 			<div class="space">
 				<div class="types">
-				<input type="text" placeholder="Socks colors">
-			 </div>
+          <label>
+            <input type="text" placeholder="Socks colors">
+          </label>
+        </div>
 
 				<div class="button-container">
-				
-					
+
+
 					<div id ="radio_buttons">
 						<div class="radio_button">
-							<input type="radio" name="choice" value="Classic uni color" class = "radio_buttons">Classic uni color
+              <label>
+                <input type="radio" name="choice" value="Classic uni color" class = "radio_buttons">
+              </label>Classic uni color
 						</div>
 						<div class="radio_button">
-							<input type="radio" name="choice" value="Classic stripes" class = "radio_buttons">Classic stripes
+              <label>
+                <input type="radio" name="choice" value="Classic stripes" class = "radio_buttons">
+              </label>Classic stripes
 						</div>
 					</div>
 
 				 </div>
 
 				<div class="address">
+          <label>
 					<input type="text" placeholder="Address">
+          </label>
 				</div>
 
 				<div class="types">
-					<input type="text" placeholder="Socks color">
-				</div>
-			 
+          <label>
+            <input type="text" placeholder="Socks color">
+          </label>
+        </div>
+
 			 </div>
-			 
+
 				<div class="submit">
 				 <input type="button" value="Submit">
 			</div>
-			
+
 		 </div>
-		 
+
 		 <div class="right">
 			<div class="info">
 				<p>All fields are mandatory to be filled. For additional information, you could call us from 12:00 - 18:00 CET, or write us an email</p>
 			</div>
-			
+
 			<div class="right-buttons">
-				
+
 				<div class="contact">
-					<a href="contact_us/contact_us_return.php" class="anchor"> 
+					<a href="contact_us.php" class="anchor">
 						<p>Contact us</p>
 					</a>
 				</div>
-				
+
 				<div class="back">
-					<a href="home_page.php" class="anchor"></a>
+					<a href="home_page.php" class="anchor">Back</a>
 				</div>
-			
+
 			</div>
 		 </div>
-		 
+
 		 <div class="sunny">
-			 <img id="photo" src="../img/sunny_logos-01.png">
-			 		
+			 <img id="photo" src="../img/sunny_logos-01.png" alt="">
+
 		<div class="submit2">
 			<a href="#">Submit</a>
 		</div>
 		</div>
 
-		
+
 	 </body>
-	
+
 </html>
